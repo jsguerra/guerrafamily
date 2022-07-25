be6ea49ccdbce303b51af77fee173e3ef53126c7
jsguerra <jose@gcostudios.com>
2022-07-25 21:03:53 +0100

    file removed

--- README.md
+++
@@ -1,4 +0,0 @@
-# Guerra Family Website
-> Private family website build
-
-Simple html5 website built in Visual Studio Code.
--- src/js/app.js
+++
@@ -1,7 +0,0 @@
-const bable = 'babel';
-
-const test = () => console.log(`this is my ${bable}`);
-
-test();
-
-console.log('another k script')
